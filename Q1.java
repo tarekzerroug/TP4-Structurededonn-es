580454bea25e5a940ab8e0f7c5cd3ad35e59441d
tarek <tarekzerroug2@gmail.com>
2025-11-30 12:31:53 -0500

    init

---
+++ Q1.java
@@ -0,0 +1,55 @@
+
+import java.util.*;
+
+public class Q1 {
+
+    public static boolean hasCycle(int[][] graph) {
+        // TODO
+        return false;
+    }
+
+    public static List<List<Integer>> findAllCycles(int[][] graph) {
+        // TODO
+        return new ArrayList<>();
+    }
+
+    public static List<List<Integer>> stronglyConnectedComponents(int[][] graph) {
+        // TODO
+        return new ArrayList<>();
+    }
+
+    public static int countReachableNodes(int[][] graph, int start) {
+        // TODO
+        return 0;
+    }
+
+    public static List<Integer> findBridgeNodes(int[][] graph) {
+        // TODO
+        return new ArrayList<>();
+    }
+
+    public static Map<Integer, Integer> getFinishingTimes(int[][] graph) {
+        // TODO
+        return new HashMap<>();
+    }
+
+    public static boolean canInstallAll(int[][] graph, List<Integer> broken) {
+        // TODO
+        return false;
+    }
+
+    public static List<Integer> findMinimalDependencySet(int[][] graph, int target) {
+        // TODO
+        return new ArrayList<>();
+    }
+
+    public static int longestDependencyChain(int[][] graph) {
+        // TODO
+        return 0;
+    }
+
+    public static List<Integer> findAllSourceNodes(int[][] graph) {
+        // TODO
+        return new ArrayList<>();
+    }
+}
---
+++ Q3.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Q3 {
+
+    public static int gameResult(int[][] graph) {
+        // TODO
+        return 0;
+    }
+
+    public static List<Integer> getMouseWinningMoves(int[][] graph) {
+        // TODO
+        return new ArrayList<>();
+    }
+
+    public static int minMovesToWin(int[][] graph, int player) {
+        // TODO
+        return -1;
+    }
+
+    public static int gameResultFrom(int[][] graph, int mousePos, int catPos, boolean mouseTurn) {
+        // TODO
+        return 0;
+    }
+
+    public static List<String> getAllDrawPositions(int[][] graph) {
+        // TODO
+        return new ArrayList<>();
+    }
+
+    public static Map<String, Integer> analyzeAllPositions(int[][] graph) {
+        // TODO
+        return new HashMap<>();
+    }
+
+    public static int countWinningPositions(int[][] graph, int player) {
+        // TODO
+        return 0;
+    }
+
+    public static List<Integer> findSafeNodes(int[][] graph) {
+        // TODO
+        return new ArrayList<>();
+    }
+
+}
